a0bcff9c0678789b5adbcfe3ad2c5b52a07bc4a8
obidua <64002560+obidua@users.noreply.github.com>
2025-09-08 14:10:37 +0530

    Fix Coinbase Wallet COOP Header Error

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     base: '/',
     plugins: [react()],
+    server: {
+        headers: {
+            'Cross-Origin-Opener-Policy': 'same-origin-allow-popups'
+        }
+    },
     esbuild: {
         target: 'esnext'
     },
