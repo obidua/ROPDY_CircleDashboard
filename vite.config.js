0c76b63af2ed461e16695558e0965e6eee061f10
obidua <64002560+obidua@users.noreply.github.com>
2025-09-08 13:41:14 +0530

    Updated package.json

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
     base: '/',
     plugins: [react()],
+    esbuild: {
+        target: 'esnext'
+    },
     optimizeDeps: {
         esbuildOptions: {
             target: 'esnext'
